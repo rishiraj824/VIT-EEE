3693c4cde29cba3f2966de99973db061c5296a87
Rishi Raj <rishiraj824@gmail.com>
2016-08-29 03:54:55 +0530

    added

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -25,20 +25,121 @@ app.controller('NoticeCtrl', function($scope){
     type : "Notice",
     title : "VITEEE 2016",
     date : "31 December 2016",
-    class : "blue"
-  },
-  {
-    type : "Announcement",
-    title : "VITEEE 2016",
-    date : "31 December 2016",
-    class : "green"
+    class : "blue",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod"
   },
   {
     type : "Notice",
     title : "VITEEE 2016",
     date : "31 December 2016",
-    class : "pink"
+    class : "pink",
+    description: " quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
+  },
+  {
+    type : "Notice",
+    title : "VITEEE 2016",
+    date : "31 December 2016",
+    class : "yellow",
+    description: " quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
   }];
 
+});
+  
+   app.controller('CourseModalCtrl',  function ($scope, $uibModal, $log) {
+
+    $scope.courses = [{
+      name: "B.Tech in Computer Science & Engineering",
+      school: "School of Copmuter Science & Engineering",
+      class : "blue-small",
+      link : "http://vit.ac.in/scope"
+    },
+    {
+      name: "B.Tech in Computer Science & Engineering",
+      school: "School of Copmuter Science & Engineering",
+      class : "blue-small",
+      link : "http://vit.ac.in/scope"
+    },
+    {
+      name: "MCA in Computer Science & Engineering",
+      school: "School of Copmuter Science & Engineering",
+      class : "pink-small",
+      link : "http://vit.ac.in/scope"
+    },
+    {
+      name: "M.Tech in Computer Science & Engineering",
+      school: "School of Copmuter Science & Engineering",
+      class : "blue-small",
+      link : "http://vit.ac.in/scope"
+    },
+    {
+      name: "B.Tech in Computer Science & Engineering",
+      school: "School of Copmuter Science & Engineering",
+      class : "yellow-small",
+      link : "http://vit.ac.in/scope"
+    },
+    {
+      name: "BBA in Computer Science & Engineering",
+      school: "School of Copmuter Science & Engineering",
+      class : "pink-small",
+      link : "http://vit.ac.in/scope"
+    },
+    {
+      name: "B.Tech in Computer Science & Engineering",
+      school: "School of Copmuter Science & Engineering",
+      class : "yellow-small",
+      link : "http://vit.ac.in/scope"
+    },
+    {
+      name: "B.Tech in Computer Science & Engineering",
+      school: "School of Copmuter Science & Engineering",
+      class : "blue-small",
+      link : "http://vit.ac.in/scope"
+    },
+    {
+      name: "B.Tech in Computer Science & Engineering",
+      school: "School of Copmuter Science & Engineering",
+      class : "yellow-small",
+      link : "http://vit.ac.in/scope"
+    }
+    ]
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+ 
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+});
+    
+   app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+ 
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
 });
 })();
